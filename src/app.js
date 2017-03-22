af7d69925a57cc074533942cd218a3ce5113f7dc
angel munoz <angelmsanchez85@gmail.com>
2017-03-22 17:41:04 +0100

    agree put method change import app main

--- src/app.js
+++ src/app.js
@@ -20,11 +20,13 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-// Import Models and controllers
+// Import Models
 var tvshowModel = require('./models/tvshow.model')(app, mongoose);
 var loginModel = require('./models/login.model')(app, mongoose);
 var tradeReportModel = require('./models/trade-report.model')(app, mongoose);
 var db = require('./mocks/db');
+
+// Import Controllers
 var TVShowCtrl = require('./controllers/tvshow.controller');
 var loginController = require('./controllers/login.controller');
 var tradeReportController = require('./controllers/trade-report.controller');
@@ -39,38 +41,39 @@ router.route('/login')
   .post(loginController.login);
 
 router.route('/trade-reports')
-  .get(tradeReportController.listTradeReport);
+  .get(tradeReportController.listTradeReport)
+  .post(tradeReportController.addTradeReport)
+  .put(tradeReportController.cancelTradeReport);
 
 router.route('/trade-reports/:tradeId')
   .get(tradeReportController.detailTradeReport);
-
 
 router.route('/esis')
   .get(TVShowCtrl.findEsis, function (req, res) {
     res.send(" esis!");
   });
+
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+  
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(router);
 
-// API routes
-var tvshows = express.Router();
-
-tvshows.route('/tvshows')
-  .get(TVShowCtrl.findAllTVShows)
-  .post(TVShowCtrl.addTVShow);
-
-tvshows.route('/esis')
-  .get(TVShowCtrl.findEsis, function (req, res) {
-    res.send("Hello world esis!");
-  });
-
-tvshows.route('/tvshows/:id')
-  .get(TVShowCtrl.findById)
-  .put(TVShowCtrl.updateTVShow)
-  .delete(TVShowCtrl.deleteTVShow);
-
-app.use('/api', tvshows);
-
 // Start server
-app.listen(5555, function () {
-  console.log("Node server running on http://localhost:5555");
+app.listen(4444, function () {
+  console.log("Node server running on http://localhost:4444");
 });
--- src/controllers/trade-report.controller.js
+++ src/controllers/trade-report.controller.js
@@ -8,15 +8,38 @@ exports.listTradeReport = function (req, res) {
   res.status(200).jsonp(tradeReportsMock);
 };
 
+//POST - Return all tradeReports 
+exports.addTradeReport = function (req, res) {
+  console.log('POST TRADE-REPORT');
+  req.body.id = Math.random();
+  tradeReportsMock.elements.push(req.body);
+  tradeReportsMock.total = tradeReportsMock.elements.length;
+  res.status(200).jsonp(req.body);
+};
+
+//PUT - Return all tradeReports 
+exports.cancelTradeReport = function (req, res) {
+  console.log('PUT TRADE-REPORT');
+  req.body.status = 2;
+  tradeReportsMock['elements'].map(tradeReport => {
+    if (tradeReport.tradeId === req.body.tradeId) {
+      return req.body;
+    } else {
+      return tradeReport;
+    }
+  });
+  res.status(200).jsonp(req.body);
+};
+
 //GET - Return all tradeReports 
 exports.detailTradeReport = function (req, res) {
-  console.log('GET DETAIL TRADE REPORT ', req.params.tradeId);
+  console.log('GET DETAIL TRADE-REPORT ', req.params.tradeId);
   let tradeReportFind;
   tradeReportsMock['elements'].map(tradeReport => {
-    console.log('dentro .map');
     if (tradeReport.tradeId === req.params.tradeId) {
       tradeReportFind = tradeReport;
     }
   });
+  tradeReportFind = tradeReportFind ? tradeReportFind : {};
   res.status(200).jsonp(tradeReportFind);
 };
